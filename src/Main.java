bc31e977e53aa8d888af28488b9591fa39f6451d
Ester Alves <esteralves.s@icloud.com>
2022-07-21 22:10:45 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,62 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("Curso java");
-        curso1.setDescricao("Descrição do Curso Java");
-        curso1.setCargaHoraria(60);
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("Curso JavaScript");
-        curso2.setDescricao("Descrição do Curso JavaScript");
-        curso2.setCargaHoraria(35);
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
 
         Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("Mentoria de Java");
-        mentoria.setDescricao("Descrição da Mentoria Java");
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devEster = new Dev();
+        devEster.setNome("Ester");
+        devEster.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Ester:" + devEster.getConteudosInscritos());
+        devEster.progredir();
+        devEster.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Ester:" + devEster.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Ester:" + devEster.getConteudosConcluidos());
+        System.out.println("XP:" + devEster.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devCaio = new Dev();
+        devCaio.setNome("Caio");
+        devCaio.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Caio:" + devCaio.getConteudosInscritos());
+        devCaio.progredir();
+        devCaio.progredir();
+        devCaio.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Caio:" + devCaio.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos Caio:" + devCaio.getConteudosConcluidos());
+        System.out.println("XP:" + devCaio.calcularTotalXp());
+
     }
 }
